0b3cbc63c60bee5466dc9a48428dca343e0f1f5e
Angelika Gavrileja <Gavrileja@Angelikas-MacBook-Air.local>
2022-03-22 22:01:07 +0200

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Assignment: Work on a basic program that prints out 25% of what ever you pass into it.
+    // Polymer bottle degradation by weight (grams)
+        float weight_before_degradation = 18.9843f ;
+        System.out.println("Weight after degradation: " + (weight_before_degradation * 0.75));
+
+
+    }
+}
